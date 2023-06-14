425a2f5c2a100ae9d280cf56162192b4f6ce5217
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-14 13:51:12 +0300

    refactor: catsModule to another file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './controller/cats.controller';
-import { CatsService } from './service/cats.service';
+import { CatsModule } from "./module/cats.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/controller/cats.controller.ts
+++ src/controller/cats.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CatsService } from '../service/cats.service';
 
-@Controller()
+@Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  @Get(`/cats`)
+  @Get()
   getCats(): string {
     return this.catsService.getCats();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `this return the cat with ${id}`;
   }
 }
