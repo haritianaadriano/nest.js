0d51eeb161aeb18c7ecbc83ad335bab5e7dbe56a
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-14 09:20:48 +0300

    chore: first controller without data just string

---
+++ src/controller/cats.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from '../service/cats.service';
+
+@Controller()
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get(`/cats`)
+  getCats(): string {
+    return this.catsService.getCats();
+  }
+}
