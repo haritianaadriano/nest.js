8c903c3c371a5201d31e86a88ce583117269ecdf
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-17 09:16:20 +0300

    feat: .env config in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from "./module/cats.module";
+import { CatsModule } from './module/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
