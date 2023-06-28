b9705d855e2b601d40235020bfdd80e5fda004ca
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-28 10:41:47 +0300

    feat: database module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './module/cats.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './module/database.module';
 
 @Module({
-  imports: [CatsModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [CatsModule, DatabaseModule, ConfigModule.forRoot({ 
+    validationSchema: Joi.object({
+      POSTGRES_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_USER: Joi.string().required(),
+      POSTGRES_PASSWORD: Joi.string().required(),
+      POSTGRES_DB: Joi.string().required(),
+      PORT: Joi.number()
+    })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
