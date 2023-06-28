daf5c36ad574d98b47c9820fdf4366af87842c2d
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-28 11:23:56 +0300

    fix: .env to match with the app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configSerivce = app.get(ConfigService);
+  await app.listen(configSerivce.get('PORT') || 3000);
 }
+dotenv.config();
 bootstrap();
