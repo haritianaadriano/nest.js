43655fbf1678cf1840b09ce9dd119b6802ef9b44
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-01-10 14:26:43 +0300

    fix: 404 not found when accessing health

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './module/cats.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './controller/health.controller';
 
 @Module({
   imports: [CatsModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
